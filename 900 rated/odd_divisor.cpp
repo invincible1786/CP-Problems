8861f58ea6f24a14ed86eeef81486ebfd6a360ab
[invincible1786] <ruhaankakar1786.24@kgpian.iitkgp.ac.in>
2026-01-17 13:42:52 +0530

    shouldn't be a power of 2

---
+++ 900 rated/odd_divisor.cpp
@@ -0,0 +1,15 @@
+// link - https://codeforces.com/problemset/problem/1475/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long n;
+        cin >> n;
+        while(n%2 == 0) n /= 2;
+        cout << (n>1 ? "YES\n" : "NO\n");
+    }    
+}
