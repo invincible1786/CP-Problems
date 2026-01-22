1c766e314ccc195c64b041fab132007838c68f00
[invincible1786] <ruhaankakar1786.24@kgpian.iitkgp.ac.in>
2026-01-22 14:37:23 +0530

    imp theory

---
+++ 1000 rated/double_ended_strings.cpp
@@ -0,0 +1,21 @@
+// link - https://codeforces.com/problemset/problem/1506/C
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string a, b;
+        cin >> a >> b;
+        int n = a.size(), m = b.size();
+        int same = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                
+            }
+        }
+        cout << m + n - 2 * same << endl;
+    }
+}
---
+++ 1000 rated/traffic_light.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+
+        if (n % 2 == 0) {
+            cout << n / 2 << " " << n / 2 << endl;
+            continue;
+        }
+
+        long long spf = n;
+        for (long long i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                spf = i;
+                break;
+            }
+        }
+
+        long long a = n / spf;
+        long long b = n - a;
+
+        cout << a << " " << b << endl;
+    }
+    return 0;
+
+}
