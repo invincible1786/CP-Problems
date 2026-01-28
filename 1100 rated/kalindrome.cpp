a18c3255c8390e2e87a524c28e55b4b5aaa6a6f1
[invincible1786] <ruhaankakar1786.24@kgpian.iitkgp.ac.in>
2026-01-28 22:57:35 +0530

    spot on approach

---
+++ 1100 rated/kalindrome.cpp
@@ -0,0 +1,47 @@
+// link -> https://codeforces.com/problemset/problem/1610/B
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool check(int lft, int rt, vector<int>& arr, int skip){
+    while(lft < rt){
+            if(arr[lft] == skip) lft += 1;
+            else if(arr[rt] == skip) rt -= 1;
+            else if(arr[lft] == arr[rt]){
+                lft += 1;
+                rt -= 1;
+            }
+            else return false;
+        } 
+        return true;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n,-1);
+        for(int i=0; i<n; i++) cin >> arr[i];
+
+        if(n == 1) cout << "YES" << endl;
+
+        else{
+        int lft = 0, rt = n-1;
+        while(arr[lft] == arr[rt]){
+            lft += 1;
+            rt -= 1;
+        }
+
+        if(lft > rt) cout << "Yes" << endl;
+
+        else{
+        bool flg = (check(lft,rt,arr,arr[lft]) || check(lft,rt,arr,arr[rt]));
+
+        if(flg) cout << "Yes" << endl;
+        else cout << "No" << endl;
+            }
+        } 
+    }
+}
